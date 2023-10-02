87c28eac2b54760f8ae671cd28ec98368c9089c4
M G Asheque Mahmood <mahmood.asheque@gmail.com>
2023-10-02 20:14:36 +0300

    messages_app

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
-import { serve } from "./deps.js";
+import { sql } from "./database/database.js";
+import { configure, renderFile, serve } from "./deps.js";
 
-const handleRequest = (request) => {
-  return new Response("Hello world!");
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
+
+const responseDetails = {
+  headers: { "Content-Type": "text/html;charset=UTF-8" },
+};
+
+const create = async (sender, message) => {
+  await sql`INSERT INTO messages (sender, message)
+    VALUES (${ sender }, ${ message })`;
+};
+
+const findLastFiveMessages = async () => {
+  return await sql`SELECT * FROM messages ORDER BY id DESC LIMIT 5`;
+};
+
+const listMessages = async () => {
+  const data = {
+    messages: await findLastFiveMessages(),
+  };
+
+  return new Response(await renderFile("index.eta", data), responseDetails);
+};
+
+const addMessage = async (request) => {
+  const formData = await request.formData();
+  const sender = formData.get("sender");
+  const message = formData.get("message");
+
+  await create(sender, message);
+};
+
+const handleRequest = async (request) => {
+  if (request.method === "GET") {
+    return await listMessages();
+  } else if (request.method === "POST") {
+    await addMessage(request);
+    return redirectTo("/");
+  }
 };
 
 serve(handleRequest, { port: 7777 });
